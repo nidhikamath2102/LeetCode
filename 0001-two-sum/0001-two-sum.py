22339187096fc5c592128dfc3343e5d2bfb60c0f
Nidhi Govindraya Kamath <52463216+nidhikamath2102@users.noreply.github.com>
2025-03-09 00:19:57 -0500

    Time: 3 ms (51.32%), Space: 19 MB (24.56%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        arr={}
+        for i,v in enumerate(nums):
+            arr[v] = i
+        
+        for i,v in enumerate(nums):
+            if target-v in arr and i!=arr[target-v]:
+                return [i, arr[target-v]]
+                
