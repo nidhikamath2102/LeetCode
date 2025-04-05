a830f2f8315ee9b83a1f58929ebd33be08333ebd
Nidhi Govindraya Kamath <52463216+nidhikamath2102@users.noreply.github.com>
2025-04-05 01:07:25 -0400

    Time: 23 ms (6.92%), Space: 18 MB (83.79%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -2,15 +2,12 @@ import re
 class Solution:
     def checkAlphaNumeric(self, c):
         return ('0'<=c.lower()<='9' or 'a'<=c.lower()<='z')
-        
+
     def isPalindrome(self, s: str) -> bool:
         i,j=0,len(s)-1
         while i<=j:
-            while i<len(s)-1 and not self.checkAlphaNumeric(s[i]):
-                i+=1
-            while j>0 and not self.checkAlphaNumeric(s[j]):
-                j-=1
+            while i<j and not self.checkAlphaNumeric(s[i]): i+=1
+            while j>i and not self.checkAlphaNumeric(s[j]): j-=1
             if i<=j and s[i].lower()!=s[j].lower(): return False
-            i+=1
-            j-=1
+            i,j=i+1,j-1
         return True
