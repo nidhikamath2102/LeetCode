92bcb242020731d1440afdf413d7108ab3bf61cf
Nidhi Govindraya Kamath <52463216+nidhikamath2102@users.noreply.github.com>
2025-04-06 01:24:34 -0400

    Time: 596 ms (47.74%), Space: 20.8 MB (23.4%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,10 +5,10 @@ class Solution:
             if i>0 and s[i]==s[i-1]: 
                 i+=1
                 continue
-            t,j,k=s[i],i+1,len(s)-1
+            j,k=i+1,len(s)-1
             while j<k:
-                sum=s[j]+s[k]
-                if t+sum==0: 
+                sum=s[i]+s[j]+s[k]
+                if sum==0: 
                     arr.append([s[i], s[j], s[k]])
                     while j<k and s[j]==s[j+1]:
                         j+=1
@@ -16,7 +16,7 @@ class Solution:
                         k-=1
                     k-=1
                     j+=1
-                elif t+sum>0: k-=1
+                elif sum>0: k-=1
                 else: j+=1
             i+=1
         return arr
