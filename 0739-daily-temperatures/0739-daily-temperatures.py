ddca496bff508a908501d3bfc71c6f90052e0c6d
Nidhi Govindraya Kamath <52463216+nidhikamath2102@users.noreply.github.com>
2025-04-10 01:44:28 -0400

    Time: 5158 ms (5%), Space: 27.1 MB (79.99%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -3,16 +3,11 @@ class Solution:
         answer, n = [0], len(temperatures)-1
         st=[n]
         for i in range(n-1,-1,-1):
+            while len(st)>0 and temperatures[i]>=temperatures[st[-1]]: 
+                st.pop()
+
+            if len(st)>0: answer.insert(0,st[-1]-i)
+            else: answer.insert(0, 0)
+            st.append(i)
             
-            if temperatures[i]<temperatures[st[-1]]:
-                answer.insert(0,st[-1]-i)
-                st.append(i)
-                print("Inside If = ", temperatures[i], temperatures[st[-1]], st[-1])
-            else:
-                while len(st)>0 and temperatures[i]>=temperatures[st[-1]]: 
-                    st.pop()
-                if len(st)>0: answer.insert(0,st[-1]-i)
-                else: answer.insert(0, 0)
-                st.append(i)
-                print("Inside else = ", temperatures[i], temperatures[st[-1]], st[-1])
         return answer
