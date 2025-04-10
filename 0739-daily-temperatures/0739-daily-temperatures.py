889f01ad7ddb766bd2ccacab60af95b532eb2807
Nidhi Govindraya Kamath <52463216+nidhikamath2102@users.noreply.github.com>
2025-04-10 01:56:15 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,13 +1,10 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        answer, n = [0], len(temperatures)-1
-        st=[n]
+        n = len(temperatures)-1
+        answer,st=[0]*n,[n]
         for i in range(n-1,-1,-1):
             while len(st)>0 and temperatures[i]>=temperatures[st[-1]]: 
                 st.pop()
-
-            if len(st)>0: answer.insert(0,st[-1]-i)
-            else: answer.insert(0, 0)
+            if len(st)>0: answer[i]=st[-1]-i
             st.append(i)
-            
         return answer
